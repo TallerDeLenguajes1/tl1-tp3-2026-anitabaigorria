68a43fc0835d7750c3db64ffb76766faf4df0fad
anitabaigorria <abaigorriafacet@gmail.com>
2026-05-05 14:34:07 -0300

    guardo cambios en rama 'main', terminada funcion BuscarNombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 50
 
 void MostrarPersonas(char *V[]);
 // void MostrarPersonas1(char **V[],int N);
+void BuscarNombre(char *V[], char palabra[]);
 
 // VERSION PUNTERO SIMPLE (tamaño estático de vector principal (5) y tamaño de cada elemento (nombre) asignado dinámicamente)
 int main(){
     char *V[5], buffer[MAX];
+    int id;
     
     for (int i = 0; i < 5; i++)
     {
-        printf("Ingrese el nombre: \n");
+        printf("Ingrese el nombre: ");
         fflush(stdin);
         gets(buffer);
+        printf("\n");
 
         V[i] = (char *)malloc((strlen(buffer)+1)*sizeof(char)); 
         strcpy(V[i],buffer);
@@ -23,17 +27,24 @@ int main(){
 
     MostrarPersonas(V);
     
+    printf("Ingrese palabra clave a buscar: ");
+    fflush(stdin);
+    gets(buffer);
+    printf("\n");
+    
+    BuscarNombre(V,buffer);
+    
     //  LIBERO NOMBRES ALMACENADOS DINÁMICAMENTE
     for (int i = 0; i < 5; i++) {
     free(V[i]);
-    }
+}
 }
 
 // VERSION CON PUNTERO DOBLE (tamaño de vector principal y de cada elemento (nombre) asignado dinámicamente)
 // int main(){
 //     char **V,buffer[MAX];
 //     int N;
-    
+
 //     // pedir cantidad de nombres a cargar
 //     printf("Cantidad de nombres a cargar: \n");
 //     scanf("%d",&N);
@@ -43,9 +54,9 @@ int main(){
 
 //     for (int i = 0; i < N; i++)
 //     {
-//         printf("Ingrese el nombre: \n");
-//         fflush(stdin);
-//         gets(buffer);
+    //         printf("Ingrese el nombre: \n");
+    //         fflush(stdin);
+    //         gets(buffer);
 
 //         V[i] = (char *)malloc((strlen(buffer)+1)*sizeof(char)); 
 //         strcpy(V[i],buffer);
@@ -56,7 +67,7 @@ int main(){
 //         MostrarPersonas1(V[j],N);
 //     }
     
-    
+
 //     //  LIBERO NOMBRES ALMACENADOS DINÁMICAMENTE
 //     for (int i = 0; i < 5; i++) {
 //     free(V[i]);
@@ -70,8 +81,10 @@ int main(){
 void MostrarPersonas(char *V[]){
     for (int i = 0; i < 5; i++)
     {
+        printf("[%d] ", i);
         puts(V[i]);
     }    
+    printf("\n");
 }
 
 // void MostrarPersonas1(char **V[],int N){
@@ -81,3 +94,22 @@ void MostrarPersonas(char *V[]){
 //     }  
 // }
 
+void BuscarNombre(char *V[], char palabra[]){
+    int bandera = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (strstr(V[i],palabra) != NULL)
+        {
+            printf("PALABRA '%s' ENCONTRADA DENTRO DE '%s'!\n",palabra,V[i]);
+
+            bandera = 1;
+            break;
+        }
+    }
+
+    if (bandera == 0)
+    {
+        printf("-1\n");
+    }
+    
+}
